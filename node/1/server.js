42518178a918e27dfe6137de00cceea8c44afc0f
Midhun Manohar <midhun@toobler.com>
2014-07-03 16:01:49 +0530

    Docmntn for ray 1

--- node/1/server.js
+++ node/1/server.js
@@ -1,42 +1,53 @@
+ /***
+* Merxz
+*
+* Author                : Jumana Shireen (jumana.shireen14@gmail.com)
+* Company               : Toobler
+* Email:                : info@toobler.com
+* Web site              : http://www.toobler.com
+* Created               : 3/July/2014
+* Description           : Server for managing post method
+* ==============================================================================================
+* Change History:
+* ----------------------------------------------------------------------------------------------
+* Sl.No.  Date   Author   Description
+* ----------------------------------------------------------------------------------------------
+*
+* 
+*/
 var express = require('express');
 var bodyParser = require('body-parser');
-var qs = require('qs');
+
 //var url = require( "url" );
 
 var app = express();
-
+// set port number to be communicated with
 app.set('port', process.env.PORT || 8547);
 
 
-app.use(bodyParser.urlencoded())
+app.use(bodyParser.urlencoded());
 
 // parse application/json
-app.use(bodyParser.json())
+app.use(bodyParser.json());
 
 // parse application/vnd.api+json as json
-app.use(bodyParser.json({ type: 'application/vnd.api+json' }))
+app.use(bodyParser.json({ type: 'application/vnd.api+json' }));
+
 
 
 /*
-app.set('views', path.join(“/directory/”, 'views'));
-app.set('view engine', 'jade');
-app.get('/', function(req, res){
-req.params('name')
-});
+ *function for handling post request
+ *@param path
+ *param function callback(Request,Response)
 */
-
-
 app.post('/test/:number', function(req,res){
-	//console.log('ddd');
-	console.log(req.body)
-	//var name = req.body.check,
-       // color= req.body.checkAnother;
-	// var theUrl = url.parse( req.url );
-	//var queryObj = qs.parse( theUrl.query );
-	 //var obj = JSON.parse( queryObj.jsonData );
+	
+	console.log(req.body);
+	
+	// returns response in valid json format.
 	res.json({number : req.params.number, user :  req.query.user, check : req.body.check, checkAnother : req.body.checkAnother } );
 });
 
-
+//listen for request at specified port number
 app.listen(8547);
 
